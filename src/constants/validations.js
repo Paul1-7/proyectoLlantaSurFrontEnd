4edc36c098d65158103a813757c3ba0293c6fb62
Paul <moicoxoxo@gmail.com>
2023-01-07 11:26:25 -0400

    update regex alphanumeric

--- src/constants/validations.js
+++ src/constants/validations.js
@@ -20,7 +20,7 @@ setLocale({
 });
 
 const regex = {
-  alphaNumeric: /^$|^[a-zA-Z\d\-_./\séáíóúñ,+]+$/,
+  alphaNumeric: /^$|^[a-zA-Z\d\-_./\séáíóúñ°"',+]+$/,
   password: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}|)/,
   number: /^[0-9]+$/,
   tel: /^[0-9-+\s]+$/,
