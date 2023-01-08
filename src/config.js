7a778ee511a6ef48ea88f591cf3a19239fb482bc
Paul <moicoxoxo@gmail.com>
2023-01-07 22:36:13 -0400

    change url to https

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   // BASE_URL: 'http://localhost:3000'
-  BASE_URL: 'http://llanta-sur.up.railway.app'
+  BASE_URL: 'https://llanta-sur.up.railway.app'
 };
 
 export default config;
