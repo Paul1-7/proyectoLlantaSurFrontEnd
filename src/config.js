404e81e62d72c36fa88ae62ec2727183b32315e6
Paul <moicoxoxo@gmail.com>
2023-02-02 12:15:05 -0400

    add env var

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
+const API_URL_BASE = import.meta.env.VITE_API_URL_BASE;
+const API_URL_DEV = import.meta.env.VITE_API_URL_DEV;
+const isProd = process.env.NODE_ENV === 'production';
+
 const config = {
-  BASE_URL: 'http://localhost:3000',
-  // BASE_URL: 'https://llanta-sur.up.railway.app'
+  BASE_URL: isProd ? API_URL_BASE : API_URL_DEV,
 };
 
 export default config;
