f78f2c85849e77cf07a7f3bde221cc12faed7888
Paul <moicoxoxo@gmail.com>
2023-02-05 10:29:36 -0400

    add plugin pwa

---
+++ pwa.js
@@ -0,0 +1,31 @@
+export const pwaOptions = {
+  mode: 'production',
+  base: '/',
+  includeAssets: ['favicon/*.png', 'favicon/*.ico'],
+  manifest: {
+    name: 'Llanta Sur',
+    short_name: 'Llanta Sur',
+    description: 'Tienda de repuestos automotrices y todo tipo de llantas',
+    display: 'fullscreen',
+    theme_color: '#000000',
+    background_color: '#ffffff',
+    icons: [
+      {
+        src: 'favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
+import replace from '@rollup/plugin-replace';
 import process from 'process';
+import { pwaOptions } from './pwa';
+
+const replaceOptions = { __DATE__: new Date().toISOString(), preventAssignment: true };
+const claims = process.env.CLAIMS === 'true';
+const reload = process.env.RELOAD_SW === 'true';
+const selfDestroying = process.env.SW_DESTROY === 'true';
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src';
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts';
+  pwaOptions.strategies = 'injectManifest';
+  pwaOptions.manifest.name = 'PWA Inject Manifest';
+  pwaOptions.manifest.short_name = 'PWA Inject';
+}
+
+if (claims) pwaOptions.registerType = 'autoUpdate';
+
+if (reload) {
+  replaceOptions.__RELOAD_SW__ = 'true';
+}
+
+if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(pwaOptions), replace(replaceOptions)],
   resolve: {
     alias: {
       '~': path.resolve(process.cwd(), './src'),
