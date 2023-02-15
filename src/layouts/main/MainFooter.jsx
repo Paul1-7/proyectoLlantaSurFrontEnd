7eb239151808cc327c7bef7380cffb621b119cb0
Paul <moicoxoxo@gmail.com>
2023-02-15 17:49:43 -0400

    add links to page

--- src/layouts/main/MainFooter.jsx
+++ src/layouts/main/MainFooter.jsx
@@ -1,45 +1,32 @@
-import { Icon } from '@iconify/react';
-import googleFill from '@iconify/icons-eva/google-fill';
-import twitterFill from '@iconify/icons-eva/twitter-fill';
-import facebookFill from '@iconify/icons-eva/facebook-fill';
-import linkedinFill from '@iconify/icons-eva/linkedin-fill';
-import { Link as ScrollLink } from 'react-scroll';
+import { Link as ScrollLink, animateScroll } from 'react-scroll';
 import { Link as RouterLink } from 'react-router-dom';
 // material
-import { Grid, Link, Divider, Container, Typography, IconButton, Stack, styled } from '@mui/material';
+import { Grid, Link, Divider, Container, Typography, IconButton, Stack, styled, Box } from '@mui/material';
 //
 import Logo from '~/components/Logo';
+import { Facebook, WhatsApp } from '@mui/icons-material';
 
 // ----------------------------------------------------------------------
 
 const SOCIALS = [
-  { name: 'FaceBook', icon: facebookFill },
-  { name: 'Google', icon: googleFill },
-  { name: 'Linkedin', icon: linkedinFill },
-  { name: 'Twitter', icon: twitterFill },
+  { name: 'FaceBook', icon: <Facebook /> },
+  { name: 'Whatsapp', icon: <WhatsApp /> },
 ];
 
 const LINKS = [
   {
-    headline: 'Minimal',
+    headline: 'Tienda',
     children: [
-      { name: 'About us', href: '#' },
-      { name: 'Contact us', href: '#' },
-      { name: 'FAQs', href: '#' },
+      { name: 'Sobre nosotros', href: '#' },
+      { name: 'Preguntas frecuentes', href: '#' },
     ],
   },
   {
-    headline: 'Legal',
+    headline: 'Politicas',
     children: [
-      { name: 'Terms and Condition', href: '#' },
-      { name: 'Privacy Policy', href: '#' },
-    ],
-  },
-  {
-    headline: 'Contact',
-    children: [
-      { name: 'support@minimals.cc', href: '#' },
-      { name: 'Los Angeles, 359  Hidden Valley Road', href: '#' },
+      { name: 'Garantias', href: '#' },
+      { name: 'Devoluciones y cambios', href: '#' },
+      { name: 'Términos y condiciones', href: '#' },
     ],
   },
 ];
@@ -59,28 +46,21 @@ export default function MainFooter() {
         <Grid
           container
           justifyContent={{ xs: 'center', md: 'space-between' }}
+          alignItems="center"
           sx={{ textAlign: { xs: 'center', md: 'left' } }}
         >
-          <Grid item xs={12} sx={{ mb: 3 }}>
-            <ScrollLink to="move_top" spy smooth>
-              <Logo sx={{ mx: { xs: 'auto', md: 'inherit' } }} />
-            </ScrollLink>
-          </Grid>
           <Grid item xs={8} md={3}>
-            <Typography variant="body2" sx={{ pr: { md: 5 } }}>
-              The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI
-              ©, ready to be customized to your style.
-            </Typography>
+            <Box>
+              <Logo sx={{ mx: { xs: 'auto' } }} />
+              <Typography variant="body2" align="center">
+                Mantén tu vehículo seguro y en óptimo estado con nuestros repuestos
+              </Typography>
+            </Box>
 
-            <Stack
-              spacing={1.5}
-              direction="row"
-              justifyContent={{ xs: 'center', md: 'flex-start' }}
-              sx={{ mt: 5, mb: { xs: 5, md: 0 } }}
-            >
+            <Stack spacing={1.5} direction="row" justifyContent={{ xs: 'center' }} sx={{ mt: 5, mb: { xs: 5, md: 0 } }}>
               {SOCIALS.map((social) => (
                 <IconButton key={social.name} color="primary" sx={{ p: 1 }}>
-                  <Icon icon={social.icon} width={16} height={16} />
+                  {social.icon}
                 </IconButton>
               ))}
             </Stack>
@@ -121,10 +101,10 @@ export default function MainFooter() {
             mt: 10,
             pb: 5,
             fontSize: 13,
-            textAlign: { xs: 'center', md: 'left' },
+            textAlign: { xs: 'center' },
           }}
         >
-          © 2021. All rights reserved
+          © {new Date().getFullYear()} Todos los derechos reservados
         </Typography>
       </Container>
     </RootStyle>
