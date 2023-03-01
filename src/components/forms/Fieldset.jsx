b1c5d35a7c2a6df4b418d5b630c5f3480028f551
Paul <moicoxoxo@gmail.com>
2023-03-01 15:14:23 -0400

    fix component props

--- src/components/forms/Fieldset.jsx
+++ src/components/forms/Fieldset.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { grey } from '@mui/material/colors';
 
-const style = { border: `1px solid ${grey['700']}`, padding: '24px', borderRadius: '10px' };
+const styleByDefault = { border: `1px solid ${grey['700']}`, padding: '24px', borderRadius: '10px' };
 
-function Fieldset({ title, children, ...other }) {
+function Fieldset({ title, children, style, ...other }) {
   return (
-    <fieldset style={{ ...style, ...other }}>
+    <fieldset style={{ ...styleByDefault, ...style }} {...other}>
       <Typography variant="subtitle1" align="center" component="legend" sx={{ padding: '0 16px' }}>
         {title}
       </Typography>
@@ -20,5 +20,5 @@ export default Fieldset;
 Fieldset.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node,
-  other: PropTypes.object,
+  style: PropTypes.object,
 };
